ca63e333283d033ec3344f62e7983dd2a6f577b1
Greg ORIOL <greg@gregoriol.net>
2025-07-31 17:16:08 +0200

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyPickerPopover",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "SwiftyPickerPopover",
+            targets: ["SwiftyPickerPopover"]),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .target(
+            name: "SwiftyPickerPopover",
+            dependencies: [],
+            path: "SwiftyPickerPopover",
+            exclude: ["Info.plist"],
+            resources: [
+                .process("Base.lproj"),
+                .process("en.lproj"),
+                .process("ja.lproj"),
+                .process("AbstractPopover.storyboard"),
+                .process("ColumnStringPickerPopover.storyboard"),
+                .process("CountdownPickerPopover.storyboard"),
+                .process("DatePickerPopover.storyboard"),
+                .process("StringPickerPopover.storyboard"),
+            ]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
